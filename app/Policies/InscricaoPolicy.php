dcee65292084dda0afcb1fc6f43249eef13f3303
Vitor Targino <vitortargino@MacBook-Air-de-Vitor.local>
2025-05-01 04:49:54 -0300

    Organizador como classe removido e politicas adicionadas

--- app/Policies/InscricaoPolicy.php
+++ app/Policies/InscricaoPolicy.php
@@ -9,33 +9,28 @@ class InscricaoPolicy
 {
     public function viewAny(User $user)
     {
-        // Qualquer usuário pode visualizar suas próprias inscrições
         return in_array($user->tipo, ['inscrito', 'organizador', 'administrador']);
     }
 
     public function view(User $user, Inscricao $inscricao)
     {
-        // Usuário pode visualizar se for o dono da inscrição ou organizador
         return $user->id === $inscricao->user_id || $user->tipo === 'organizador' || $user->tipo === 'administrador';
         ;
     }
 
     public function create(User $user)
     {
-        // Apenas inscritos podem criar inscrições
         return $user->tipo === 'inscrito' || $user->tipo === 'administrador';
     }
 
     public function update(User $user, Inscricao $inscricao)
     {
-        // Apenas organizadores podem atualizar inscrições
         return $user->tipo === 'organizador' || $user->tipo === 'administrador';
         ;
     }
 
     public function delete(User $user, Inscricao $inscricao)
     {
-        // Apenas organizadores podem deletar inscrições
         return $user->tipo === 'organizador' || $user->tipo === 'administrador';
         ;
     }
--- app/Policies/PagamentoPolicy.php
+++ app/Policies/PagamentoPolicy.php
@@ -9,32 +9,27 @@ class PagamentoPolicy
 {
     public function viewAny(User $user)
     {
-        // Qualquer usuário pode visualizar seus próprios pagamentos
         return in_array($user->tipo, ['inscrito', 'organizador', 'administrador']);
     }
 
     public function view(User $user, Pagamento $pagamento)
     {
-        // Usuário pode visualizar se for o dono do pagamento ou organizador
         return $user->id === $pagamento->user_id || $user->tipo === 'organizador' || $user->tipo === 'administrador';
         ;
     }
 
     public function create(User $user)
     {
-        // Apenas inscritos podem criar pagamentos
         return $user->tipo === 'inscrito' || $user->tipo === 'administrador';
     }
 
     public function update(User $user, Pagamento $pagamento)
     {
-        // Apenas organizadores podem atualizar pagamentos
         return $user->tipo === 'organizador' || $user->tipo === 'administrador';
     }
 
     public function delete(User $user, Pagamento $pagamento)
     {
-        // Apenas organizadores podem deletar pagamentos
         return $user->tipo === 'organizador' || $user->tipo === 'administrador';
     }
 }
